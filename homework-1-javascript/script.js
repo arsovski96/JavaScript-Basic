c5fdfff93d357bd699c8d77548253e8bde4a382a
arsovski96 <117305795+arsovski96@users.noreply.github.com>
2022-12-06 19:32:42 +0100

    Added code homework-exercise-1

---
+++ homework-1-javascript/script.js
@@ -0,0 +1,5 @@
+var percent = (5/100)*119.95;
+var onePhone = percent + 119.95;
+var totalPhones = 30* onePhone;
+
+console.log(`The total price of 30 phones with tax included is: ${totalPhones} dollars`)
